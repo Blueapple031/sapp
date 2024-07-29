b7ef2af8f575e07a785ec83880948811e9d6a9bb
Blueapple031 <ksy1001k96@gmail.com>
2024-07-29 14:11:57 +0900

    work

---
+++ src/test/java/com/ll/helloJava/HelloJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.helloJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
